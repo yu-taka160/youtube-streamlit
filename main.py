f68544595eb6479f4e7fc07e50c37436f7752d75
yumi takahashi <yumiu0821@icloud.com>
2026-08-19 18:08:35 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+import time
+
+st.title('Streamlit 超入門')
+
+st.write('プレグレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration{i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+'Done!!!!'   
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+expander1 = st.expander('問い合わせ1')
+expander1.write('問い合わせ1の回答')
+expander2 = st.expander('問い合わせ2')
+expander2.write('問い合わせ2の回答')
+expander3 = st.expander('問い合わせ3')
+expander3.write('問い合わせ3の回答')
+
+# text = st.text_input('あなたの趣味を教えてください。')
+# condition = st.slider('あなたの今の調子は？',0, 100, 50)
+
+# 'あなたの趣味：', text
+# 'コンディション:', condition
+
+# if st.checkbox('Show Image'):
+#     img = Image.open('子どものころ.jpg')
+#     st.image(img,caption='Yumi',use_column_width=True)
+
